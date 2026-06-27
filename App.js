22b35328f6bec28b2dafe6059f3b6d1f17d0c81f
arkanhussienmashi-del <arkan.hussien.mashi@gmail.com>
2026-06-27 20:39:13 +0300

    Add allowFontScaling=false globally + responsive StyleSheet fixes

--- App.js
+++ App.js
@@ -31,6 +31,11 @@ import * as DocumentPicker from 'expo-document-picker';
 import NetInfo from '@react-native-community/netinfo';
 import * as Crypto from 'expo-crypto';
 
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
+
 const API_URL = 'https://server-ten-wheat.vercel.app';
 
 async function apiRequest(method, path, body) {
